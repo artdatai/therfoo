765190ad295c381ddfe26b0f6ea91e2539760ef9
David Ndungu <dnjuguna@gmail.com>
2019-03-24 01:27:58 -0700

    refactor crossentropy loss

--- losses/crossentropy.go
+++ losses/crossentropy.go
@@ -5,23 +5,15 @@ import (
 	"math"
 )
 
-func CrossEntropy(yTrue, yEstimate *tensor.Vector) *tensor.Vector {
-	n := yTrue.Len()
-	l := make(tensor.Vector, n, n)
-	for i := 0; i < n; i++ {
-		l[i] = -(*yTrue)[i]*math.Log((*yEstimate)[i]) - (1-(*yTrue)[i])*math.Log(1-(*yEstimate)[i])
-		if math.IsNaN(l[i]) {
-			l[i] = 0.
-		}
+func CrossEntropy(yEstimate, yTrue []float32) (sum float32) {
+	for i := range yTrue {
+		sum += -yTrue[i]*float32(math.Log(float64(yEstimate[i]))) - (1-yTrue[i])*float32(math.Log(float64(1-yEstimate[i])))
 	}
-	return &l
+	return
 }
 
-func CrossEntropyPrime(yTrue, yEstimate *tensor.Vector) *tensor.Vector {
-	n := yTrue.Len()
-	d := make(tensor.Vector, n, n)
-	for i := 0; i < n; i++ {
-		d[i] = (*yEstimate)[i] - (*yTrue)[i]
+func CrossEntropyPrime(yEstimate, yTrue, delta []float32) {
+	for i := 0; i < len(yTrue); i++ {
+		delta[i] += yEstimate[i] - yTrue[i]
 	}
-	return &d
 }
